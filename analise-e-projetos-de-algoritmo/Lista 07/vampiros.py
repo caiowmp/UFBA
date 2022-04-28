9447ee5063c85653752a7d3d03ef93f89e23b170
Caio <contato.caiomp@gmail.com>
2022-04-28 19:54:02 -0300

    Acept

--- analise-e-projetos-de-algoritmo/Lista 07/vampiros.py
+++ analise-e-projetos-de-algoritmo/Lista 07/vampiros.py
@@ -1,6 +1,3 @@
-#caso o valor do dado seja menor ou igual ao ataque o vamp1 vence.
-#quem perder, é subtraído da vida o dano e quem agnahr é somado à vida o dano.
-
 while True:
 
     vidaVamp1,vidaVamp2,ataque,dano = map(int,input().split())
@@ -8,35 +5,24 @@ while True:
     if vidaVamp1 == 0 and vidaVamp2 == 0 and ataque == 0 and dano == 0:
         break
 
-    #qual a probabilidadae do vamp1 vencer dados os valores ?
+    pDadoV1 = ataque/6
+    pDadoV2 = 1 - pDadoV1
 
-    probabilidadeDadoV1 = (ataque*100)/6
-    #print(probabilidadeDadoV1)
-    probabilidadeDadoV2 = 100 - probabilidadeDadoV1
-    #print(probabilidadeDadoV2)
+    hitsVamp1 = 0
+    hitsVamp2 = 0
+
+    while vidaVamp1 > 0:
+      vidaVamp1-=dano
+      hitsVamp1+=1
+
+    while vidaVamp2 > 0:
+      vidaVamp2-=dano
+      hitsVamp2+=1
 
     #probabilidade com a moeda justa
-    if probabilidadeDadoV1 == 50:
-        probabilidadeTotal = vidaVamp1/(vidaVamp1+vidaVamp2)
-    elif probabilidadeDadoV1 == 0:
-        probabilidadeTotal = 0
-    elif probabilidadeDadoV1 == 100:
-        probabilidadeTotal == 100
+    if pDadoV1 == 0.5:
+        pTotal = 100*hitsVamp1/(hitsVamp1+hitsVamp2)
     else: #probabilidade com moeda injusta
-        if dano == 1:
-            probabilidadeTotal = (1-(probabilidadeDadoV2/probabilidadeDadoV1)**vidaVamp1)/(1-(probabilidadeDadoV2/probabilidadeDadoV1)**(vidaVamp1+vidaVamp2))
-        else:
-            hitsTomaveisVamp1 = 0
-            hitsTomaveisVamp2 = 0
+            pTotal = 100*(1-(pDadoV2/pDadoV1)**hitsVamp1)/(1-(pDadoV2/pDadoV1)**(hitsVamp1+hitsVamp2))
 
-            while vidaVamp1 > 0:
-              vidaVamp1-=dano
-              hitsTomaveisVamp1+=1
-
-            while vidaVamp2 > 0:
-              vidaVamp2-=dano
-              hitsTomaveisVamp2+=1
-
-            probabilidadeTotal = (1-(probabilidadeDadoV2/probabilidadeDadoV1)**hitsTomaveisVamp1)/(1-(probabilidadeDadoV2/probabilidadeDadoV1)**(hitsTomaveisVamp1+hitsTomaveisVamp2))
-
-    print(round(probabilidadeTotal*100, 1))
+    print(round(pTotal, 1))
