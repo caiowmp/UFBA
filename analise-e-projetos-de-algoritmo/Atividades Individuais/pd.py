cf267355b4d6dad48fa4e776e01ce1d42a7aa666
Caio <contato.caiomp@gmail.com>
2022-05-05 18:03:04 -0300

    Questão completa

--- analise-e-projetos-de-algoritmo/Atividades Individuais/pd.py
+++ analise-e-projetos-de-algoritmo/Atividades Individuais/pd.py
@@ -14,18 +14,10 @@ def funcao_ingenua(n):
         return funcao_ingenua(n-1) + funcao_ingenua(n-3) + funcao_ingenua(n-4)
 
 #forma com programação dinâmica top down
-memo = {}
+memo = {1:1,2:1,3:3,4:4}
 def funcao_pd(n):
     if n in memo:
         return memo[n]
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 1
-    elif n == 3:
-        return 3
-    elif n == 4:
-        return 4
     else:
         memo[n] = funcao_pd(n-1) + funcao_pd(n-3) + funcao_pd(n-4)
         return memo[n]
