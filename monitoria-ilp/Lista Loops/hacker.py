b4f178e51ca759594767fa660da4a12f0982a68e
caiowmp <contato.caiomp@gmail.com>
2022-05-19 20:13:29 -0300

    Adicionando prints para melhor compreensão do código

--- monitoria-ilp/Lista Loops/hacker.py
+++ monitoria-ilp/Lista Loops/hacker.py
@@ -4,12 +4,16 @@ contador = 0
 
 for i in range(inicio,fim+1):
     multiplos = 0
+    #print("Numero sendo anlisado:",i)
 
-    for j in range(2,i+1):
+    for j in range(1,i+1):
+        print("Verificando se",i,"é divisível por",j)
         if i % j == 0:
-            multiplos +=1
-
-    if(multiplos == 1):        
-        contador +=1
-        
+            multiplos += 1
+            print("Multiplo encontrado:",multiplos)
+  
+    if multiplos == 2:
+        contador += 1
+        print("Número primo encontrado:",contador)
+    
 print(contador)
