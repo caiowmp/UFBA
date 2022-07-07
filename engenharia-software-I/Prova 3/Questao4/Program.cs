f2698b990c279a184987af156479dc37e8e70fc4
caiowmp <contato.caiomp@gmail.com>
2022-07-06 22:07:15 -0300

    SorveteMaker

---
+++ engenharia-software-I/Prova 3/Questao4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Questao4
+{
+    class SorveteMaker
+    {
+        static void Main(string[] args)
+        {
+            //criar um Sorvete
+            Sorvete Sorvete = new SorveteSimples();
+            //coloca calda no sorvete
+            Sorvete = new Calda(Sorvete);
+            Sorvete = new Calda(Sorvete);
+            //coloca passas no sorvete
+            Sorvete = new Passas(Sorvete);
+            Console.WriteLine("Preco -->" + Sorvete.getPreco.ToString());
+            Console.WriteLine("Descricao --> "+ Sorvete.getDescricao.TrimEnd(' ', ','));         
+            Console.ReadLine();
+        }
+    }
+}
+
