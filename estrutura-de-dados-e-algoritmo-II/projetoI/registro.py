3849ac39d51c01ffeb7d2d3b1c2a01a8bcdd91db
caiowmp <contato.caiomp@gmail.com>
2022-10-11 14:45:54 -0300

    Simplificando a inserção e leitura dos registros

--- estrutura-de-dados-e-algoritmo-II/projetoI/registro.py
+++ estrutura-de-dados-e-algoritmo-II/projetoI/registro.py
@@ -6,4 +6,4 @@ class Registro:
         self.idade = idade
 
     def __repr__ (self):
-        return "Chave " + str(self.chave) + ", Nome " + str(self.nome) + ", Idade " + str(self.idade)
+        return str(self.chave) + " " + str(self.nome) + " " + str(self.idade) + '\n'
