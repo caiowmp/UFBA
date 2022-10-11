376ed7f49efb1f1ee3bbabf9a2c967829bb747ae
caiowmp <contato.caiomp@gmail.com>
2022-10-11 12:15:44 -0300

    arquivo representante da Arvore B+

---
+++ estrutura-de-dados-e-algoritmo-II/projetoI/arvoreBp.py
@@ -0,0 +1,28 @@
+from registro import Registro
+from arquivo import Arquivo
+
+class ArvoreBp:
+
+    def __init__ (self, GRAU_MINIMO_INDICE, FATOR_CONJSEQ):
+        self.MAX_INDICES_NO = GRAU_MINIMO_INDICE * 2 -1
+        self.MIN_INDICES_NO = GRAU_MINIMO_INDICE - 1 
+        self.MAX_REG_NO = FATOR_CONJSEQ * 2 - 1
+        self.MIN_REG_NO = FATOR_CONJSEQ - 1
+        Arquivo.criaConjuntoSequencia()
+        Arquivo.criaIndices()
+        
+    def insereRegistro (self, registro):
+        if isinstance (registro, Registro):
+            Arquivo.insereDadosConjuntoSequencia(registro)
+
+    def consultaRegistro (self, chave):
+        pass
+
+    def removeRegistro (self, chave):
+        pass
+
+    def imprimeArvore (self):
+        pass
+
+    def imprimeChaves (self):
+        pass
