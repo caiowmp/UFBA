b59bc604d2b1c7894eee2f502b7200f715eefd1e
caiowmp <contato.caiomp@gmail.com>
2022-10-12 19:42:59 -0300

    Consertando o endereço dos arquivos

--- estrutura-de-dados-e-algoritmo-II/projetoI/arquivo.py
+++ estrutura-de-dados-e-algoritmo-II/projetoI/arquivo.py
@@ -5,43 +5,39 @@ class Arquivo:
 
     @classmethod
     def criaIndices (cls):
-        file = open("indices","w+")
+        file = open("projetoI\\indices","w+")
         file.close()
 
     @classmethod
     def criaConjuntoSequencia (cls):
-        file = open("conjuntoSequencia","w+")
+        file = open("projetoI\\conjuntoSequencia","w+")
         file.close()
 
     @classmethod
     def insereDadosConjuntoSequencia (cls, registro):
-        file = open('conjuntoSequencia','r')
+        file = open('projetoI\\conjuntoSequencia','r')
         conteudo = file.readlines()
-        '''if isinstance(registro, Registro):
-            conteudo.append("Chave " + str(registro.chave) + ", ")
-            conteudo.append("Nome " + str(registro.nome) + ", ")
-            conteudo.append("Idade " + str(registro.idade) + '\n')'''
         conteudo.append(repr(registro))
 
-        file = open('conjuntoSequencia','w')
+        file = open('projetoI\\conjuntoSequencia','w')
         file.writelines(conteudo)
         file.close()
 
     @classmethod
     def insereDadosIndices (cls, registro):
-        file = open('indices','r')
+        file = open('projetoI\\indices','r')
         conteudo = file.readlines()
         if isinstance(registro, Registro):
             conteudo.append("Chave: " + str(registro.chave))
 
-        file = open('indices','w')
+        file = open('projetoI\\indices','w')
         file.writelines(conteudo)
         file.close()
 
     @classmethod
     def leConjuntoSequencia (cls):
         conjuntoSequencia = []
-        with open('conjuntoSequencia') as arquivo:
+        with open('projetoI\\conjuntoSequencia') as arquivo:
             for linha in arquivo:
                 linha = linha.split(' ')
                 conjuntoSequencia.append(linha)
