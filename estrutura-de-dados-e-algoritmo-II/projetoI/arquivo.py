866d916a5f789a1deb1eba20b7e1f4e512ae0855
caiowmp <contato.caiomp@gmail.com>
2022-10-13 18:47:05 -0300

    Ajeitando o caminho do arquivo

--- estrutura-de-dados-e-algoritmo-II/projetoI/arquivo.py
+++ estrutura-de-dados-e-algoritmo-II/projetoI/arquivo.py
@@ -5,36 +5,36 @@ class Arquivo:
 
     @classmethod
     def criaIndices (cls):
-        file = open("projetoI\\indices","w+")
+        file = open("indices","w+")
         file.close()
 
     @classmethod
     def criaConjuntoSequencia (cls):
-        file = open("projetoI\\conjuntoSequencia","w+")
+        file = open("conjuntoSequencia","w+")
         file.close()
 
     @classmethod
     def insereDadosConjuntoSequencia (cls, conteudo):
 
-        file = open('projetoI\\conjuntoSequencia','w')
+        file = open('conjuntoSequencia','w')
         file.writelines(conteudo)
         file.close()
 
     @classmethod
     def insereDadosIndices (cls, registro):
-        file = open('projetoI\\indices','r')
+        file = open('indices','r')
         conteudo = file.readlines()
         if isinstance(registro, Registro):
             conteudo.append("Chave: " + str(registro.chave))
 
-        file = open('projetoI\\indices','w')
+        file = open('indices','w')
         file.writelines(conteudo)
         file.close()
 
     @classmethod
     def leConjuntoSequencia (cls):
         conjuntoSequencia = []
-        with open('projetoI\\conjuntoSequencia') as arquivo:
+        with open('conjuntoSequencia') as arquivo:
             for linha in arquivo:
                 linha = linha.split(' ')
                 conjuntoSequencia.append(linha)
