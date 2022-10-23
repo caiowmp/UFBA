e6de1e66a525eb3385be4f20d5db18b33ecf6aca
caiowmp <contato.caiomp@gmail.com>
2022-10-23 13:55:39 -0300

    .

--- estrutura-de-dados-e-algoritmo-II/projetoI/registro.py
+++
@@ -1,9 +0,0 @@
-class Registro:
-
-    def __init__ (self, chave: int, nome, idade: int):
-        self.chave = chave
-        self.nome = nome
-        self.idade = idade
-
-    def __repr__ (self):
-        return str(self.chave) + " " + self.nome+ " " + str(self.idade) + ' \n'
